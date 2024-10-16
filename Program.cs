da341a5362be481a88ef7ab3592d7ee3aad2a47c
pcy-soo <hyannerel87@gmail.com>
2024-10-16 15:17:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defaultRegex = @"^[a-z]+$";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write($"Enter a regular expression (or press ENTER to use the default '{defaultRegex}'): ");
+                string inputRegex = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(inputRegex))
+                {
+                    inputRegex = defaultRegex;
+                }
+
+                Regex r;
+                try
+                {
+                    r = new Regex(inputRegex);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Invalid regular expression: {inputRegex}");
+                    continue;
+                }
+
+                Console.Write("Enter some input: ");
+                string userInput = Console.ReadLine();
+
+               
+                bool isMatch = r.IsMatch(userInput);
+                Console.WriteLine($"{userInput} matches {inputRegex}? {isMatch}");
+
+               
+                Console.WriteLine("Press ESC to end or any key to try again.");
+                var key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                Console.WriteLine();  
+            }
+        }
+    }
+}
